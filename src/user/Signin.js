ced1d94e7ed74623504b2e590bb1949f76e5e906
Arpan Dutta <smalldutta@gmail.com>
2021-04-29 13:34:20 +0300

    adds API key

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../core/Layout";
+import {API} from '../config';
 
 const Signin = () => (
     <Layout title="Signin" description="Signin to Node React E-commerce App">
-        ...
+        
     </Layout>
 );
 
